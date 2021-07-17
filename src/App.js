5c23b1dff541cdd2446a9e806ba14054b6a9704d
Christen Nguyen <christennguyen@Christens-MBP-2.attlocal.net>
2021-07-16 17:33:27 -0700

    fix: fix build issues

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css'
 import styled from 'styled-components'
 
@@ -17,21 +18,21 @@ function App() {
                 <div className="machine">
                     <div className="machine-roof">
                         <div className="machine-silencer">
-                            <div className="machine-smoke"></div>
+                            <div className="machine-smoke" />
                         </div>
                     </div>
                     <div className="machine-main">
-                        <div className="machine-body"></div>
-                        <div className="machine-back"></div>
-                        <div className="machine-front"></div>
+                        <div className="machine-body" />
+                        <div className="machine-back" />
+                        <div className="machine-front" />
                     </div>
                 </div>
                 <div className="road">
                     <div className="mud-wrap clearfix">
-                        <div className="mud"></div>
-                        <div className="mud"></div>
-                        <div className="mud"></div>
-                        <div className="mud"></div>
+                        <div className="mud" />
+                        <div className="mud" />
+                        <div className="mud" />
+                        <div className="mud" />
                     </div>
                 </div>
             </div>
